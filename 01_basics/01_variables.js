18c85c60c2b73a632200050b3a7ad58645161b1f
Sabit143 <118983881+Sabit143@users.noreply.github.com>
2023-12-08 09:19:53 +0000

    Some more code added

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,24 @@
+const accountId = 14453;
+let accountEmail = "sabit123@gmail.com";
+var accountPassword = "12345";
+accountCity = "jaipur";
+//accountId = 2;
+accountPassword = "67688";
+accountCity = "Bengalur";
+console.log(accountId);
+let accountState;
+console.table([accountEmail,accountId, accountPassword,accountCity,accountState]);
+/*
+prefer not to use var 
+because of issue in block scope and functional scope 
+*/
+// number => 2 to power 53
+// bigint (used for the higher number)
+// string => true/false
+// undefined =>
+let temparature;// this will results in the undefined 
+temparature = null; // we will prefer temparature as  null instead od zero , cause  zero has a temparature value 
+//null=> standalone
+// symbol => used in the uniqueness 
+//symbol
+console.log(typeof "sabit");
